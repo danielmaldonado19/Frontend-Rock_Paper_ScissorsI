4fdc6404b0454e4c48434394449a616f2b22b348
Daniel Maldonado <damaldy19@hotmail.com>
2022-07-09 17:22:38 -0500

    app: computer choice, user choices & result stored in const

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert('hola');
+//1.Capture the span elms regarding to computer & user choice. The same has to be done with result.
+
+const computerChoice = document.getElementById('computer-choice').innerHTML;
+const userChoice = document.getElementById('user-choice');
+
+const resultDisplay = document.getElementById('result');
