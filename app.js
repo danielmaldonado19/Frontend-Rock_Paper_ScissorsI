f1a2f2ccbe627fd82e8d6067979208bc3e1f96ee
Daniel Maldonado <damaldy19@hotmail.com>
2022-07-09 17:39:16 -0500

    app: get the possible options and store them into a const

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-//1.Capture the span elms regarding to computer & user choice. The same has to be done with result.
+/*1.Capture the span elms regarding to computer & user choice. The same has to be done with result.*/
 
 const computerChoice = document.getElementById('computer-choice').innerHTML;
 const userChoice = document.getElementById('user-choice');
 
 const resultDisplay = document.getElementById('result');
+
+
+//2.Get all the possible options.
+
+const possibleOptions = document.querySelectorAll('button');
+/*.querySelectorAll returns the class/element we called as the param of the method.*/
