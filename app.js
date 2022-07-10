cafa56a74672cf1f287e2878658dcf85a05496cf
Daniel Maldonado <damaldy19@hotmail.com>
2022-07-09 19:18:56 -0500

    app: computer choice finished

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*1.Capture the span elms regarding to computer & user choice. The same has to be done with result.*/
 
-const computerChoiceDisplay = document.getElementById('computer-choice').innerHTML;
+const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 
 const resultDisplay = document.getElementById('result');
@@ -18,8 +18,30 @@ let userChoice;
 
 possibleOptions.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
-    console.log(e.target)
-    userChoiceDisplay.innerHTML = userChoice
+    userChoiceDisplay.innerHTML = userChoice;
+    /*for each one of the elements of the list possibleOptions it triggers a fn -> possibleChoice param contain the btn and when we click any of the 3 btns it activates a fn which creates a pointer event (its an object) named e (automatically created by JS) -> the fn stores in userChoice let the str (given by e.target.id) of the btn we clicked.-> Then show the id of the btn we clicked in the h2 of the user.*/
+
+    //4.Generate the computer choice.
+    generateComputerChoice();
+    
 }))
-/*for each one of the elements of the list possibleOptions it triggers a fn -> possibleChoice param contain the btn and when we click any of the 3 btns it activates a fn which creates a pointer event (its an object) named e (automatically created by JS) -> the fn stores in userChoice let the str (given by e.target.id) of the btn we clicked.-> Then show the id of the btn we clicked in the h2 of the user.
-*/
+
+
+//4.
+
+let computerChoice;
+
+function generateComputerChoice() {
+    const randomNumber = Math.ceil(Math.random() * possibleOptions.length); //3 options.
+    
+    if(randomNumber === 1){
+        computerChoice = 'rock';
+    }
+    else if(randomNumber === 2){
+        computerChoice = 'papper';
+    }
+    else if(randomNumber === 3){
+        computerChoice = 'scissors';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
